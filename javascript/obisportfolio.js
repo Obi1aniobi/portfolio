f87fca41bdc3c5f43ea0d7e818674e03d6ecf80f
aniobiobinna85@yahoo.com <aniobiobinna85@yahoo.com>
2023-07-18 21:55:55 +0200

    portfolio project

---
+++ javascript/obisportfolio.js
@@ -0,0 +1,24 @@
+document.getElementById('downloadButton').addEventListener("click", alertThem);
+
+	function alertThem(){
+
+		alert("I'm still under construction");
+
+		}
+
+document.getElementById('cookbookButton').addEventListener("click", alertThem);
+
+	function alertThem(){
+
+		alert("I'm still under construction");
+
+		}
+
+
+document.getElementById('wireframeButton').addEventListener("click", alertThem);
+
+	function alertThem(){
+
+		alert("I'm still under construction");
+
+		}
